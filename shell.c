1d4ce6d72cfa66289c950d335f6e5da354a76608
chris <christorper@gmail.com>
2025-08-17 14:28:21 -0700

    parse a line of input into an array

--- shell.c
+++ shell.c
@@ -3,15 +3,35 @@
 
 int main() {
   char buffer[20];
+  char *token;
   char *tokens[25];
 
+  int tokenCount = 0;
+
   while (1) {
-    if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-      char *token = strtok(buffer, " ");
-      printf("%s", buffer);
+    if (fgets(buffer, sizeof(buffer), stdin)) {
+
+      if (*buffer == 'q') {
+        break;
+      }
+
+      tokenCount = 0;
+      token = strtok(buffer, " ");
+
+      while (token != NULL) {
+        tokens[tokenCount] = token;
+        token = strtok(NULL, " ");
+        tokenCount++;
+      }
+
+      for (int i = 0; i < tokenCount; ++i) {
+        printf("%s\n", tokens[i]);
+      }
 
     } else {
       printf("Error, can't read input");
     }
   }
+
+  printf("made it here\n");
 }
