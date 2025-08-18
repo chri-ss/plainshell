68d585fc851837a88acf190e26901fab16fde988
chris <christorper@gmail.com>
2025-08-17 19:08:58 -0700

    fork a new process

--- shell.c
+++ shell.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
+
+#define buf_size 1024
+#define tokens_lim 100
 
 int main() {
-  char buffer[20];
+  char buffer[buf_size];
   char *token;
-  char *tokens[25];
+  char *tokens[tokens_lim];
 
   int tokenCount = 0;
 
@@ -15,23 +22,45 @@ int main() {
         break;
       }
 
+      // tokenize input
       tokenCount = 0;
       token = strtok(buffer, " ");
 
       while (token != NULL) {
-        tokens[tokenCount] = token;
+        tokens[tokenCount] = strdup(token);
         token = strtok(NULL, " ");
         tokenCount++;
       }
+      tokens[tokenCount] = NULL;
 
-      for (int i = 0; i < tokenCount; ++i) {
-        printf("%s\n", tokens[i]);
+      // fork a new process to run the program in
+      int status;
+      pid_t childPid = fork();
+      pid_t waitResult;
+
+      if (childPid < 0) {
+        perror("fail to fork");
+        exit(1);
+      } else if (childPid == 0) {
+        printf("pid = %d\nchild_pid = %d", getpid(), childPid);
+
+        // split arguments from program name
+        char *args[tokenCount - 1];
+
+        for (int i = 1; i < tokenCount; i++) {
+          args[i - 1] = strdup(tokens[i]);
+          printf("%s", args[i]);
+        }
+        // execute the program if it exists
+
+        execvp(tokens[0], args);
+      } else {
+        waitResult = waitpid(childPid, &status, WUNTRACED);
+        printf("complete\n");
       }
 
     } else {
       printf("Error, can't read input");
     }
   }
-
-  printf("made it here\n");
 }
